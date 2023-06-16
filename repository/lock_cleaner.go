d2a52cc84170a1312b0ddaa8967c8a4cc732fb0e
Fredrik Appelberg <fredrik.appelberg@tt.se>
2023-06-16 16:18:52 +0200

    More linting

--- repository/lock_cleaner.go
+++ repository/lock_cleaner.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ttab/elephantine/pg"
 )
 
-func (p *PGDocStore) RunCleaner(ctx context.Context, period time.Duration) {
+func (s *PGDocStore) RunCleaner(ctx context.Context, period time.Duration) {
 	for {
 		select {
 		case <-time.After(period):
@@ -17,18 +17,18 @@ func (p *PGDocStore) RunCleaner(ctx context.Context, period time.Duration) {
 			return
 		}
 
-		jobLock, err := pg.NewJobLock(p.pool, p.logger, "cleaner",
+		jobLock, err := pg.NewJobLock(s.pool, s.logger, "cleaner",
 			10*time.Second, 1*time.Minute, 20*time.Second, 5*time.Second)
 		if err != nil {
-			p.logger.ErrorCtx(ctx, "failed to create job lock",
+			s.logger.ErrorCtx(ctx, "failed to create job lock",
 				elephantine.LogKeyError, err)
 
 			continue
 		}
 
-		err = jobLock.RunWithContext(ctx, p.removeExpiredLocks)
+		err = jobLock.RunWithContext(ctx, s.removeExpiredLocks)
 		if err != nil {
-			p.logger.ErrorCtx(
+			s.logger.ErrorCtx(
 				ctx, "lock cleaner error",
 				elephantine.LogKeyError, err,
 			)
@@ -36,15 +36,14 @@ func (p *PGDocStore) RunCleaner(ctx context.Context, period time.Duration) {
 	}
 }
 
-func (p *PGDocStore) removeExpiredLocks(ctx context.Context) error {
-	p.logger.Debug("removing expired document locks")
+func (s *PGDocStore) removeExpiredLocks(ctx context.Context) error {
+	s.logger.Debug("removing expired document locks")
 
-	err := p.reader.DeleteExpiredDocumentLocks(ctx,
+	err := s.reader.DeleteExpiredDocumentLocks(ctx,
 		pg.Time(time.Now().Add(5*time.Minute)),
 	)
 	if err != nil {
 		return fmt.Errorf("could not remove expired locks: %w", err)
 	}
-
 	return nil
 }
