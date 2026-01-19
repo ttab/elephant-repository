d806a8aeeafcd55445060c083e621e77c25a7d5a
Hugo Wetterberg <hugo@wetterberg.nu>
2026-01-19 08:49:12 +0100

    make the migration idempotent so that we can re-run it (no tx)

--- schema/024_index_foreign_keys.sql
+++ schema/024_index_foreign_keys.sql
@@ -4,11 +4,11 @@
 -- updates will fail.
 
 -- Not used anymore
-DROP TABLE acl_audit;
+DROP TABLE IF EXISTS acl_audit;
 
 -- Index the planning_item foreign key to prevent table scans on cascading
 -- deletes.
-CREATE INDEX CONCURRENTLY idx_planning_assignment_planning_item
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_planning_assignment_planning_item
 ON planning_assignment(planning_item);
 
 -- Clean up any remaining eventlog publication.
